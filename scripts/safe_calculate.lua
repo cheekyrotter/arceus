10fc777449e3e50b57392611b75cdf81486f4794
cheekyrotter <george0406@outlook.com>
2025-06-07 23:01:07 +0100

    Redo safe calculate
    Safe calculate now applies to the majority of card methods that will be set by mods, am yet to test fully

--- scripts/safe_calculate.lua
+++ scripts/safe_calculate.lua
@@ -1,20 +1,35 @@
-local _calculate_joker = Card.calculate_joker
-
----@diagnostic disable-next-line: duplicate-set-field
-function Card:calculate_joker(context)
+function Arceus.safe_call(func, self, context, extra_arg)
     local result = nil
-    if self.config.center.mod then
         if Arceus.get_config_entry("safe_calc", self.config.center.mod) == true then
-            local success, result = pcall(function() _calculate_joker(self, context) end)
+            local success, result = pcall(function() return func(self, context) end)
             if not success then
                 sendErrorMessage("Caught error:"..result)
                 SMODS.calculate_effect({message = "Fatal error, removing card!", colour = G.C.RED}, self)
-                self.getting_sliced = true
-                self:start_dissolve()
+                G.E_MANAGER:add_event(Event({
+                    func = function() 
+                        self.getting_sliced = true
+                        self:start_dissolve()
+                        return true
+                    end
+                }))
             end
+            return(result)
         end
-    else
-        local result = _calculate_joker(self, context) 
-    end
     return(result)
+end
+
+
+
+local methods = {"calculate_joker", "remove", "add_to_deck", "update", "load", "calculate_dollar_bonus", "can_use_consumeable", "use_consumeable", "redeem"}
+
+for _, name in pairs(methods) do
+    local original = Card[name]
+    sendInfoMessage("ADDING METHOD ", name)
+    Card[name] = function(card, arg1, arg2)
+        if card.config.center.mod then
+            return Arceus.safe_call(original, card, arg1, arg2)
+        else
+            return original(card, arg1, arg2)
+        end
+    end
 end
