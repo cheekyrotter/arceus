c80dd55cde2170c71b02f368fee8169387a4d00f
cheekyrotter <george0406@outlook.com>
2025-06-11 21:41:09 +0100

    Prevent my mod breaking literally all other mods

--- scripts/safe_calculate.lua
+++ scripts/safe_calculate.lua
@@ -1,20 +1,17 @@
 function Arceus.safe_call(func, self, context, extra_arg)
     local result = nil
-        if Arceus.get_config_entry("safe_calc", self.config.center.mod) == true then
-            local success, result = pcall(function() return func(self, context) end)
-            if not success then
-                sendErrorMessage("Caught error:"..result)
-                SMODS.calculate_effect({message = "Fatal error, removing card!", colour = G.C.RED}, self)
-                G.E_MANAGER:add_event(Event({
-                    func = function() 
-                        self.getting_sliced = true
-                        self:start_dissolve()
-                        return true
-                    end
-                }))
+    local success, result = pcall(function() return func(self, context) end)
+    if not success then
+        sendErrorMessage("Caught error:"..result)
+        SMODS.calculate_effect({message = "Fatal error, removing card!", colour = G.C.RED}, self)
+        G.E_MANAGER:add_event(Event({
+            func = function() 
+                self.getting_sliced = true
+                self:start_dissolve()
+                return true
             end
-            return(result)
-        end
+        }))
+    end
     return(result)
 end
 
@@ -26,7 +23,7 @@ for _, name in pairs(methods) do
     local original = Card[name]
     sendInfoMessage("ADDING METHOD ", name)
     Card[name] = function(card, arg1, arg2)
-        if card.config.center.mod then
+        if card.config.center.mod and Arceus.get_config_entry("safe_calc", card.config.center.mod) == true then
             return Arceus.safe_call(original, card, arg1, arg2)
         else
             return original(card, arg1, arg2)
